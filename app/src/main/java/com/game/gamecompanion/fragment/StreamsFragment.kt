e316b4763f0f2dca2cacb37d30061c69a7f886fb
Bartomeu <bartomeu.puig@enti.cat>
2020-01-23 02:01:31 +0100

    Streams fragment finished

--- app/src/main/java/com/game/gamecompanion/fragment/StreamsFragment.kt
+++ app/src/main/java/com/game/gamecompanion/fragment/StreamsFragment.kt
@@ -2,7 +2,7 @@ package com.game.gamecompanion.fragment
 
 import android.content.Context
 import android.content.Intent
-import android.graphics.drawable.Drawable
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -14,15 +14,11 @@ import com.game.gamecompanion.R
 import com.game.gamecompanion.masktransformation.MaskTransformation
 import com.game.gamecompanion.model.TWStreamsResponse
 import com.game.gamecompanion.network.TwitchApiService
-import com.google.firebase.auth.FirebaseAuth
 import com.squareup.picasso.Picasso
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.fragment_profile.*
 import kotlinx.android.synthetic.main.fragment_streams.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import java.io.IOException
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -56,24 +52,245 @@ class StreamsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        Log.i("ProfileFragment", "++ onViewCreated ++")
+        Log.i("MainActivity", "++ onViewCreated ++")
 
 
         TwitchApiService.service.getStreams().enqueue(object : Callback<TWStreamsResponse> {
             override fun onResponse(call: Call<TWStreamsResponse>, response: Response<TWStreamsResponse>) {
                 response.body()?.data?.let { streams ->
-                    for (stream in streams) {
-                        Log.i("MainActivity", "Title: ${stream.title} and image: ${stream.imageUrl} and username: ${stream.username} and viewers: ${stream.viewerCount}")
-                        Log.i("MainActivity", "Stream Url: https://www.twitch.tv/${stream.username}")
+                    Picasso.get()
+                        .load(streams[0].imageUrl)
+                        .transform(
+                            MaskTransformation(
+                                requireContext(),
+                                20,
+                                R.drawable.ic_streams_background
+                            )
+                        )
+                        .into(ImageStream0)
 
+                    streamer0.text = streams[0].username
+                    viewers0.text = streams[0].viewerCount.toString()
+                    title_stream0.text = streams[0].title
 
-                        Picasso.get()
-                            .load(stream.imageUrl)
-                            .transform(MaskTransformation(requireContext(), 20, R.drawable.ic_streams_background))
-                            .into(ImageStream1)
+                    ImageStream0.setOnClickListener {
+                        val openURL = Intent(android.content.Intent.ACTION_VIEW)
+                        openURL.data = Uri.parse("https://www.twitch.tv/${streams[0].username}")
+                        startActivity(openURL)
+                    }
+
+                    Picasso.get()
+                        .load(streams[1].imageUrl)
+                        .transform(
+                            MaskTransformation(
+                                requireContext(),
+                                20,
+                                R.drawable.ic_streams_background
+                            )
+                        )
+                        .into(ImageStream1)
+
+                    streamer1.text = streams[1].username
+                    viewers1.text = streams[1].viewerCount.toString()
+                    title_stream1.text = streams[1].title
+
+                    ImageStream1.setOnClickListener {
+                        val openURL = Intent(android.content.Intent.ACTION_VIEW)
+                        openURL.data = Uri.parse("https://www.twitch.tv/${streams[1].username}")
+                        startActivity(openURL)
+                    }
+
+                    Picasso.get()
+                        .load(streams[2].imageUrl)
+                        .transform(
+                            MaskTransformation(
+                                requireContext(),
+                                20,
+                                R.drawable.ic_streams_background
+                            )
+                        )
+                        .into(ImageStream2)
+
+                    streamer2.text = streams[2].username
+                    viewers2.text = streams[2].viewerCount.toString()
+                    title_stream2.text = streams[2].title
+
+                    ImageStream2.setOnClickListener {
+                        val openURL = Intent(android.content.Intent.ACTION_VIEW)
+                        openURL.data = Uri.parse("https://www.twitch.tv/${streams[2].username}")
+                        startActivity(openURL)
+                    }
+
+                    Picasso.get()
+                        .load(streams[2].imageUrl)
+                        .transform(
+                            MaskTransformation(
+                                requireContext(),
+                                20,
+                                R.drawable.ic_streams_background
+                            )
+                        )
+                        .into(ImageStream2)
+
+                    streamer2.text = streams[2].username
+                    viewers2.text = streams[2].viewerCount.toString()
+                    title_stream2.text = streams[2].title
+
+                    ImageStream2.setOnClickListener {
+                        val openURL = Intent(android.content.Intent.ACTION_VIEW)
+                        openURL.data = Uri.parse("https://www.twitch.tv/${streams[2].username}")
+                        startActivity(openURL)
+                    }
+
+                    Picasso.get()
+                        .load(streams[3].imageUrl)
+                        .transform(
+                            MaskTransformation(
+                                requireContext(),
+                                30,
+                                R.drawable.ic_streams_background
+                            )
+                        )
+                        .into(ImageStream3)
+
+                    streamer3.text = streams[3].username
+                    viewers3.text = streams[3].viewerCount.toString()
+                    title_stream3.text = streams[3].title
+
+                    ImageStream3.setOnClickListener {
+                        val openURL = Intent(android.content.Intent.ACTION_VIEW)
+                        openURL.data = Uri.parse("https://www.twitch.tv/${streams[3].username}")
+                        startActivity(openURL)
+                    }
+
+                    Picasso.get()
+                        .load(streams[4].imageUrl)
+                        .transform(
+                            MaskTransformation(
+                                requireContext(),
+                                40,
+                                R.drawable.ic_streams_background
+                            )
+                        )
+                        .into(ImageStream4)
+
+                    streamer4.text = streams[4].username
+                    viewers4.text = streams[4].viewerCount.toString()
+                    title_stream4.text = streams[4].title
+
+                    ImageStream4.setOnClickListener {
+                        val openURL = Intent(android.content.Intent.ACTION_VIEW)
+                        openURL.data = Uri.parse("https://www.twitch.tv/${streams[4].username}")
+                        startActivity(openURL)
+                    }
+
+                    Picasso.get()
+                        .load(streams[5].imageUrl)
+                        .transform(
+                            MaskTransformation(
+                                requireContext(),
+                                50,
+                                R.drawable.ic_streams_background
+                            )
+                        )
+                        .into(ImageStream5)
+
+                    streamer5.text = streams[5].username
+                    viewers5.text = streams[5].viewerCount.toString()
+                    title_stream5.text = streams[5].title
+
+                    ImageStream5.setOnClickListener {
+                        val openURL = Intent(android.content.Intent.ACTION_VIEW)
+                        openURL.data = Uri.parse("https://www.twitch.tv/${streams[5].username}")
+                        startActivity(openURL)
+                    }
+
+                    Picasso.get()
+                        .load(streams[6].imageUrl)
+                        .transform(
+                            MaskTransformation(
+                                requireContext(),
+                                60,
+                                R.drawable.ic_streams_background
+                            )
+                        )
+                        .into(ImageStream6)
+
+                    streamer6.text = streams[6].username
+                    viewers6.text = streams[6].viewerCount.toString()
+                    title_stream6.text = streams[6].title
+
+                    ImageStream6.setOnClickListener {
+                        val openURL = Intent(android.content.Intent.ACTION_VIEW)
+                        openURL.data = Uri.parse("https://www.twitch.tv/${streams[6].username}")
+                        startActivity(openURL)
+                    }
+
+                    Picasso.get()
+                        .load(streams[7].imageUrl)
+                        .transform(
+                            MaskTransformation(
+                                requireContext(),
+                                70,
+                                R.drawable.ic_streams_background
+                            )
+                        )
+                        .into(ImageStream7)
+
+                    streamer7.text = streams[7].username
+                    viewers7.text = streams[7].viewerCount.toString()
+                    title_stream7.text = streams[7].title
+
+                    ImageStream7.setOnClickListener {
+                        val openURL = Intent(android.content.Intent.ACTION_VIEW)
+                        openURL.data = Uri.parse("https://www.twitch.tv/${streams[7].username}")
+                        startActivity(openURL)
+                    }
+
+                    Picasso.get()
+                        .load(streams[8].imageUrl)
+                        .transform(
+                            MaskTransformation(
+                                requireContext(),
+                                80,
+                                R.drawable.ic_streams_background
+                            )
+                        )
+                        .into(ImageStream8)
+
+                    streamer8.text = streams[8].username
+                    viewers8.text = streams[8].viewerCount.toString()
+                    title_stream8.text = streams[8].title
+
+                    ImageStream8.setOnClickListener {
+                        val openURL = Intent(android.content.Intent.ACTION_VIEW)
+                        openURL.data = Uri.parse("https://www.twitch.tv/${streams[8].username}")
+                        startActivity(openURL)
+                    }
+
+                    Picasso.get()
+                        .load(streams[9].imageUrl)
+                        .transform(
+                            MaskTransformation(
+                                requireContext(),
+                                90,
+                                R.drawable.ic_streams_background
+                            )
+                        )
+                        .into(ImageStream9)
+
+                    streamer9.text = streams[9].username
+                    viewers9.text = streams[9].viewerCount.toString()
+                    title_stream9.text = streams[9].title
+
+                    ImageStream9.setOnClickListener {
+                        val openURL = Intent(android.content.Intent.ACTION_VIEW)
+                        openURL.data = Uri.parse("https://www.twitch.tv/${streams[9].username}")
+                        startActivity(openURL)
                     }
                 }
             }
+
 
             override fun onFailure(call: Call<TWStreamsResponse>, t: Throwable) {
                 t.printStackTrace()
